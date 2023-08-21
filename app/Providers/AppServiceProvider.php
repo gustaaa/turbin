0a8e5c6a4c2140ebe9d17ff88076575ba215d023
gustaaa <gustayo24@gmail.com>
2023-08-21 07:58:00 +0700

    update waktu real

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
             // Implementasi logika validasi angka pecahan di sini
             return is_numeric($value) && strpos($value, '.') !== false;
         });
+        Schema::defaultStringLength(191); // Sesuaikan dengan kebutuhan Anda
+
+        // Mengatur zona waktu default
+        date_default_timezone_set('Asia/Jakarta'); // Ganti 'Asia/Jakarta' dengan zona waktu yang sesuai
     }
 }
