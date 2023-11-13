9e851afe57cbc102fbf8cfeaec534393945cf4a2
gustaaa <gustayo24@gmail.com>
2023-11-13 09:57:55 +0700

    fixing dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Models\Input3;
 
 class HomeController extends Controller
 {
@@ -21,8 +23,37 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        // mengambil data
+        // Get the selected date from the request
+        $selectedDate = $request->input('selected_date');
+
+        // If selected date is not provided, default to today's date
+        if (!$selectedDate) {
+            $selectedDate = now()->format('Y-m-d');
+        }
+        // Load data for hours 7 to 23
+        $input3 = Input3::whereDate('created_at', $selectedDate)
+            ->whereTime('created_at', '>=', '06:00:00')
+            ->whereTime('created_at', '<=', '23:59:59')
+            ->get();
+
+        $nextDate = Carbon::parse($selectedDate)->addDay();
+        $input3Midnight = Input3::whereDate('created_at', $nextDate)
+            ->whereTime('created_at', '>=', '00:00:00')
+            ->whereTime('created_at', '<=', '05:59:59')
+            ->get();
+
+        // Combine the data for hours 7 to 23 and hours 0 to 6
+        $home = $input3->concat($input3Midnight);
+        $tempWaterIn = $input3->concat($input3Midnight)->pluck('temp_water_in');
+        $tempWaterOut = $input3->concat($input3Midnight)->pluck('temp_water_out');
+        $tempOilIn = $input3->concat($input3Midnight)->pluck('temp_oil_in');
+        $tempOilOut = $input3->concat($input3Midnight)->pluck('temp_oil_out');
+        $batasMidnight = $input3->concat($input3Midnight)->pluck('created_at')->map(function ($datetime) {
+            return \Carbon\Carbon::parse($datetime)->modify('+1 hour')->format('H:00');
+        });
+        return view('home', compact('home', 'selectedDate', 'tempWaterIn', 'tempWaterOut', 'tempOilIn', 'tempOilOut', 'batasMidnight'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,122 @@
 @extends('layouts.app')
+
 @section('content')
 <section class="section">
     <div class="section-header">
-        <h1>Table</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="#">Components</a></div>
-            <div class="breadcrumb-item">Table</div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Title</h4>
-                </div>
-                <div class="card-body"></div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header"></div>
-                <div class="card-body"></div>
-            </div>
-        </div>
+        <h1>Chart</h1>
     </div>
     <div class="section-body">
+        <h2 class="section-title" data-id="titleInput1Management">Menu Management</h2>
+        <div class="row mb-0">
+            <div class="col-12">
+                @include('layouts.alert')
+            </div>
+        </div>
+        <div class="row row-sm">
+            <div class="col-12">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h4 data-id="input1ListData">Menu Report All
+                            <br>
+                            <span style="font-size: 13px;">Tanggal: {{ \Carbon\Carbon::parse($selectedDate)->format('d M Y') }}</span>
+                        </h4>
+                        <div class="card-header-action">
+                            <a class="btn btn-info btn-primary active search search-date">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                                Search Tanggal
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="show-search search-date mb-0" style="display: none">
+                            <form id="search-date" method="GET" action="{{ route('home') }}">
+                                <div class="form-group">
+                                    <label for="selected_date">Pilih Tanggal:</label>
+                                    <input type="date" class="form-control" id="selected_date" name="selected_date" value="{{ $selectedDate ?? now()->format('Y-m-d') }}">
+                                </div>
+                                <div class="text-right">
+                                    <button class="btn btn-primary mr-1" type="submit">Submit</button>
+                                    <a class="btn btn-secondary" href="{{ route('home') }}">Reset</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- batas -->
+    <div class="section-header d-flex">
+        <div class="flex-fill">
+            <figure class="highcharts-figure ml-5">
+                <div id="barChart"></div>
+            </figure>
+            <script src="https://code.highcharts.com/highcharts.js"></script>
+            <script>
+                // Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+                var batas = <?php echo json_encode($batasMidnight) ?>;
+                var tempWaterIn = <?php echo json_encode($tempWaterIn) ?>;
+                var tempWaterOut = <?php echo json_encode($tempWaterOut) ?>;
+                var tempOilIn = <?php echo json_encode($tempOilIn) ?>;
+                var tempOilOut = <?php echo json_encode($tempOilOut) ?>;
+                Highcharts.chart('barChart', {
+                    chart: {
+                        type: 'line'
+                    },
+                    title: {
+                        text: 'Daily Temperature'
+                    },
+                    subtitle: {
+                        text: 'Source: ' +
+                            '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
+                            'target="_blank">Wikipedia.com</a>'
+                    },
+                    xAxis: {
+                        categories: batas
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Temperature (°C)'
+                        }
+                    },
+                    plotOptions: {
+                        line: {
+                            dataLabels: {
+                                enabled: true
+                            },
+                            enableMouseTracking: false
+                        }
+                    },
+                    series: [{
+                        name: 'Temp Water In',
+                        data: tempWaterIn
+                    }, {
+                        name: 'Temp Water Out',
+                        data: tempWaterOut
+                    }, {
+                        name: 'Temp Oil In',
+                        data: tempOilIn
+                    }, {
+                        name: 'Temp Oil Out',
+                        data: tempOilOut
+                    }]
+                });
+            </script>
+        </div>
     </div>
 </section>
 @endsection
+@push('customScript')
+<script>
+    document.querySelector(".search-date").addEventListener("click", function() {
+        document.querySelector(".show-search.search-date").style.display = "block";
+        document.querySelector(".show-search.search-range").style.display = "none";
+    });
+
+    document.querySelector(".search-range").addEventListener("click", function() {
+        document.querySelector(".show-search.search-range").style.display = "block";
+        document.querySelector(".show-search.search-date").style.display = "none";
+    });
+</script>
+@endpush
